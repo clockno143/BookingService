bf22a770966ab5fba33b196733cf7b5feead7571
nikhil vishu bonageri <nikhilvb@umd.edu>
2025-12-02 14:18:39 -0500

    CORS ENABLED

--- app/main.py
+++ app/main.py
@@ -5,6 +5,7 @@ from fastapi import FastAPI
 from contextlib import asynccontextmanager
 from app.routes import router
 from app.workers import worker
+from fastapi.middleware.cors import CORSMiddleware
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
@@ -17,6 +18,15 @@ async def lifespan(app: FastAPI):
     print("App shutting down...")
 
 app = FastAPI(title="Event Booking API", lifespan=lifespan)
+
+# Add CORS middleware
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # allow all origins (or specify your frontend URL)
+    allow_credentials=True,
+    allow_methods=["*"],  # allow all HTTP methods
+    allow_headers=["*"],  # allow all headers
+)
 
 # Include your routes
 app.include_router(router)
